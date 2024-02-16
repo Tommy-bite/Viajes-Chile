8cf6e4ef5d89623291e9995c44d5bed63b1a9fc9
Cristian <cri.jimenez24@gmail.com>
2024-02-15 21:03:01 -0300

    Se agrego animación de doble click en los card

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,12 @@ $(document).ready( function() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+    $('.card-animado').dblclick(function () {
+        $(this).addClass('animate__animated animate__wobble');
+        setTimeout(() => {
+            $(this).removeClass('animate__animated animate__wobble');
+        }, 2000);
+        
+    });
+
 });
